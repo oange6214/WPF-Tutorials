8a45a07fc32da429ef8be0c6e2acc388055e4f0c
Jed <oange6214@gmail.com>
2023-06-17 08:20:18 +0800

    #7 Modals / Popups - Setting up the Modal - Creating a ModalNavigationStore

---
+++ NavigationMVVM/Stores/ModalNavigationStore.cs
@@ -0,0 +1,35 @@
+﻿using NavigationMVVM.ViewModels;
+using System;
+
+namespace NavigationMVVM.Stores;
+
+public class ModalNavigationStore
+{
+    public event Action CurrentViewModelChanged;
+
+    private ViewModelBase _currentViewModel;
+
+    public ViewModelBase CurrentViewModel
+    {
+        get => _currentViewModel;
+
+        set
+        {
+            _currentViewModel?.Dispose();
+            _currentViewModel = value;
+            OnCurrentViewModelChanged();
+        }
+    }
+
+    public bool IsOpen => CurrentViewModel != null;
+
+    public void Close()
+    {
+        CurrentViewModel = null;
+    }
+
+    private void OnCurrentViewModelChanged()
+    {
+        CurrentViewModelChanged?.Invoke();
+    }
+}
