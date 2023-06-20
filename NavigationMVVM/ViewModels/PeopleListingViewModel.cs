925154682271a608316cc1e54f22c692137866f2
Jed <oange6214@gmail.com>
2023-06-21 00:33:27 +0800

    # Adding new views - Adding functionality to the view/view model

--- NavigationMVVM/ViewModels/PeopleListingViewModel.cs
+++ NavigationMVVM/ViewModels/PeopleListingViewModel.cs
@@ -1,5 +1,20 @@
-﻿namespace NavigationMVVM.ViewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace NavigationMVVM.ViewModels;
 
 public class PeopleListingViewModel : ViewModelBase
 {
+    private readonly ObservableCollection<PersonViewModel> _people;
+
+    public IEnumerable<PersonViewModel> People => _people;
+
+    public PeopleListingViewModel()
+    {
+        _people = new ObservableCollection<PersonViewModel>();
+
+        _people.Add(new PersonViewModel("Jed"));
+        _people.Add(new PersonViewModel("Jim"));
+        _people.Add(new PersonViewModel("Bill"));
+    }
 }
